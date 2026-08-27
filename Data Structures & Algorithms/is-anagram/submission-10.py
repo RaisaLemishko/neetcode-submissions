686efe1e4f8a3b7f04727b53f55bd805405074c5
Raya Lemishko <raisa.lemishko@gmail.com>
2026-08-27 19:02:10 +0200

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counter1 = collections.Counter(s)
+        counter2 = collections.Counter(t)
+        
+        return counter1 == counter2
