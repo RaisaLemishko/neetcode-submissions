724cd4783d01d2bc94148d53aad92e0843093b2a
Raya Lemishko <raisa.lemishko@gmail.com>
2026-09-10 19:04:09 +0300

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        res = []
+        mapping = {
+            ')': '(', 
+            '}': '{', 
+            ']': '['
+            }
+        
+        if not s:
+            return True
+
+        for char in s:
+            if (char in mapping.values()):
+                res.append(char)
+            else:
+                if not res:
+                    return False
+                if (res[-1] == mapping[char]):
+                    res.pop()
+                else:
+                    return False
+        
+       
+        return True  if not res else False
